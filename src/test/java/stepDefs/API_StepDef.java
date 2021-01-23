0f00339dcad13fe565a8b7e7a7eb8f8eef445e96
Harshit Jain <harshitjaing555@gmail.com>
2021-01-24 00:47:54 +0530

    Adding API code

---
+++ src/test/java/stepDefs/API_StepDef.java
@@ -0,0 +1,34 @@
+package stepDefs;
+import java.util.List;
+import java.util.Map;
+ 
+import org.junit.Assert;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+ 
+public class API_StepDef {
+
+private static int countBefore;
+private static int countAfter; 
+
+
+ 
+@Given("A list of books are available")
+public void listOfBooksAreAvailable() {
+	
+}
+ 
+@When("I add a book via API call")
+public void addBookInList() {
+	
+}
+ 
+@Then("The book is added")
+public void bookIsAdded() {
+ 
+}
+ 
+}
